c18bc822d9063d638a81440bf4286f7b30417fe9
DancingSnow <1121149616@qq.com>
2024-06-07 18:20:14 +0800

    实现ME库存监听来通知机器检查配方

--- src/main/java/com/epimorphismmc/gregiceng/api/misc/IConfigurableAESlotList.java
+++ src/main/java/com/epimorphismmc/gregiceng/api/misc/IConfigurableAESlotList.java
@@ -3,9 +3,22 @@ package com.epimorphismmc.gregiceng.api.misc;
 import appeng.api.stacks.AEKey;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public interface IConfigurableAESlotList<K extends AEKey> {
     IConfigurableAESlot<K> getAESlot(int index);
     int getSlots();
     boolean hasConfig(@Nullable K key);
     void clearConfig();
+    default Set<K> getAEKeySet() {
+        Set<K> keys = new HashSet<>();
+        for (int i = 0; i < getSlots(); i++) {
+            var key = getAESlot(i).getConfig();
+            if (key != null) {
+                keys.add(key);
+            }
+        }
+        return keys;
+    }
 }
