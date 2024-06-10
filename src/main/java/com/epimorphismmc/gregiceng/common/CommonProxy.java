2895a56ddd8ca67950e1563435bf804f3ec10a6c
DancingSnow <60736156+DancingSnow0517@users.noreply.github.com>
2024-06-10 20:28:25 +0800

    合并拉取请求 #13
    * 添加使用闪存绑定slave逻辑

--- src/main/java/com/epimorphismmc/gregiceng/common/CommonProxy.java
+++ src/main/java/com/epimorphismmc/gregiceng/common/CommonProxy.java
@@ -3,10 +3,13 @@ package com.epimorphismmc.gregiceng.common;
 import com.epimorphismmc.gregiceng.GregicEng;
 import com.epimorphismmc.gregiceng.common.data.GECreativeModeTabs;
 import com.epimorphismmc.gregiceng.common.data.GEMachines;
+import com.epimorphismmc.gregiceng.common.item.GEDataStickBehavior;
 import com.epimorphismmc.monomorphism.proxy.base.ICommonProxyBase;
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.machine.MachineDefinition;
+import com.gregtechceu.gtceu.common.data.GTItems;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class CommonProxy implements ICommonProxyBase {
 
@@ -30,5 +33,12 @@ public class CommonProxy implements ICommonProxyBase {
     public void registerMachineDefinitions(GTCEuAPI.RegisterEvent<ResourceLocation, MachineDefinition> event) {
         GECreativeModeTabs.init();
         GEMachines.init();
+    }
+
+    /* -------------------------------------------------- Life Cycle Methods ---------------------------------------------------- */
+
+    @Override
+    public void onCommonSetupEvent(FMLCommonSetupEvent event) {
+        GTItems.TOOL_DATA_STICK.get().attachComponents(new GEDataStickBehavior());
     }
 }
